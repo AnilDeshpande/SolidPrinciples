3077caf01372bea9d07b8fa883cff664c105a22e
Anil Deshpande <anildesh82@gmail.com>
2024-07-02 11:28:32 +0530

    college updated with new modified managers

--- app/src/main/java/com/codetutor/solidprinciples/integrated/College.kt
+++ app/src/main/java/com/codetutor/solidprinciples/integrated/College.kt
@@ -4,9 +4,12 @@ import com.codetutor.solidprinciples.integrated.calculators.AcademicSalaryCalcul
 import com.codetutor.solidprinciples.integrated.calculators.SalaryCalculator
 import com.codetutor.solidprinciples.integrated.calculators.VisitingSalaryCalculator
 import com.codetutor.solidprinciples.integrated.managers.EngagementManager
+import com.codetutor.solidprinciples.integrated.managers.EngagementStrategy
+import com.codetutor.solidprinciples.integrated.managers.LectureEngagement
 import com.codetutor.solidprinciples.integrated.managers.SalaryManager
 import com.codetutor.solidprinciples.integrated.managers.StaffManager
 import com.codetutor.solidprinciples.integrated.managers.StudentManager
+import com.codetutor.solidprinciples.integrated.managers.WorkshopEngagement
 
 class College(
     val departments: List<Department>,
@@ -14,15 +17,31 @@ class College(
     val students: MutableList<Student>,
     val courses: List<Course>) {
 
-    val calculators: Map<String, SalaryCalculator> = mapOf(
-        "Academic" to AcademicSalaryCalculator(),
-        "Visiting" to VisitingSalaryCalculator()
-        // Add more entries as needed
-    )
+    private var salaryManager : SalaryManager
+    private var staffManager: StaffManager
 
-    private val salaryManager = SalaryManager(calculators)
-    private val staffManager = StaffManager(salaryManager)
-    private val studentManager = StudentManager(EngagementManager())
+    private var engagementManager: EngagementManager
+    private var studentManager: StudentManager
+
+    init {
+        val calculators: Map<String, SalaryCalculator> = mapOf(
+            "Academic" to AcademicSalaryCalculator(),
+            "Visiting" to VisitingSalaryCalculator()
+            // Add more entries as needed
+        )
+
+        val engagementStrategies: Map<String, EngagementStrategy> = mapOf(
+            "Academic" to LectureEngagement(),
+            "Visiting" to WorkshopEngagement()
+            // Add more entries as needed
+        )
+
+        salaryManager = SalaryManager(calculators)
+        staffManager = StaffManager(salaryManager)
+
+        engagementManager  = EngagementManager(engagementStrategies)
+        studentManager = StudentManager(engagementManager)
+    }
 
     fun registerStudent(student: Student) {
         studentManager.registerStudent(students, student)
