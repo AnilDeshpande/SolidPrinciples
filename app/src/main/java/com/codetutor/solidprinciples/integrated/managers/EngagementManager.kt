242c8fe8d809ac89e74acf3c61ecbad9ca5eb715
Anil Deshpande <anildesh82@gmail.com>
2024-07-02 11:28:14 +0530

    updated the engagement manager to work with engagement strategy

--- app/src/main/java/com/codetutor/solidprinciples/integrated/managers/EngagementManager.kt
+++ app/src/main/java/com/codetutor/solidprinciples/integrated/managers/EngagementManager.kt
@@ -3,8 +3,30 @@ package com.codetutor.solidprinciples.integrated.managers
 import com.codetutor.solidprinciples.integrated.Staff
 import com.codetutor.solidprinciples.integrated.Student
 
-class EngagementManager {
+interface EngagementStrategy {
+    fun engageStudents(staff: Staff, subject: String, students: List<Student>)
+}
+
+class LectureEngagement : EngagementStrategy {
+    override fun engageStudents(staff: Staff, subject: String, students: List<Student>) {
+        println("${staff.name} is lecturing on $subject")
+    }
+}
+
+class WorkshopEngagement : EngagementStrategy {
+    override fun engageStudents(staff: Staff, subject: String, students: List<Student>) {
+        println("${staff.name} is conducting a workshop on $subject")
+    }
+}
+
+class EngagementManager(private val strategies: Map<String, EngagementStrategy>) {
+    fun engageStudents(staff: Staff, subject: String, students: List<Student>) {
+        strategies[staff.type]?.engageStudents(staff, subject, students)
+            ?: throw IllegalArgumentException("No engagement strategy found for type: ${staff.type}")
+    }
+}
+/*class EngagementManager {
     fun engageStudents(staff: Staff, subject: String, students: List<Student>) {
         println("${staff.name} is engaging students in $subject")
     }
-}
+}*/
