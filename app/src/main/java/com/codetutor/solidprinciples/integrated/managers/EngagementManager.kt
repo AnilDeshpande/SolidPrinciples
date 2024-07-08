56bd1a973e706ea23607ef9657d8b80f52aa87f5
Anil Deshpande <anildesh82@gmail.com>
2024-07-08 11:29:41 +0530

    SalaryManager removed, other managers refactored

--- app/src/main/java/com/codetutor/solidprinciples/integrated/managers/EngagementManager.kt
+++ app/src/main/java/com/codetutor/solidprinciples/integrated/managers/EngagementManager.kt
@@ -5,7 +5,8 @@ import com.codetutor.solidprinciples.integrated.Student
 
 class EngagementManager(private val strategies: Map<String, EngagementStrategy>) {
     fun engageStudents(staff: Staff, subject: String, students: List<Student>) {
-        strategies[staff.type]?.engageStudents(staff, subject, students)
-            ?: throw IllegalArgumentException("No engagement strategy found for type: ${staff.type}")
+        val strategy = strategies[staff.department.name]
+            ?: throw IllegalArgumentException("No engagement strategy found for department: ${staff.department.name}")
+        staff.engageStudents(subject, students, strategy)
     }
 }
--- app/src/main/java/com/codetutor/solidprinciples/integrated/managers/StaffManager.kt
+++ app/src/main/java/com/codetutor/solidprinciples/integrated/managers/StaffManager.kt
@@ -3,13 +3,9 @@ package com.codetutor.solidprinciples.integrated.managers
 import com.codetutor.solidprinciples.integrated.Department
 import com.codetutor.solidprinciples.integrated.Staff
 
-class StaffManager(private val salaryManager: SalaryManager) {
+class StaffManager() {
     fun assignStaffToDepartment(staff: Staff, department: Department) {
         println("Assigning ${staff.name} to department: ${department.name}")
         staff.department = department
-    }
-
-    fun calculateStaffSalary(staff: Staff): Double {
-        return salaryManager.calculateSalary(staff)
     }
 }
