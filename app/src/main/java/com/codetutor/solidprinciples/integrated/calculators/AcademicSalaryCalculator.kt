9f8d76ac876ad0208bc436ed9ea22015912fd096
Anil Deshpande <anildesh82@gmail.com>
2024-07-16 16:22:29 +0530

    Salary Calculator updated

--- app/src/main/java/com/codetutor/solidprinciples/integrated/calculators/AcademicSalaryCalculator.kt
+++ app/src/main/java/com/codetutor/solidprinciples/integrated/calculators/AcademicSalaryCalculator.kt
@@ -6,4 +6,15 @@ class AcademicSalaryCalculator : SalaryCalculator {
     override fun calculateSalary(staff: Staff): Double {
         return 3000.0 // Fixed salary for academic staff
     }
+    override fun generatePaySlip(staff: Staff): String {
+        throw UnsupportedOperationException("Can't generate PaySlip")
+    }
+
+    override fun applyBonus(staff: Staff, bonus: Double): Double {
+        throw UnsupportedOperationException("Can't apply bonus")
+    }
+
+    override fun deductTaxes(staff: Staff, tax: Double): Double {
+        throw UnsupportedOperationException("Can't deduct taxes")
+    }
 }
--- app/src/main/java/com/codetutor/solidprinciples/integrated/calculators/SalaryCalculator.kt
+++ app/src/main/java/com/codetutor/solidprinciples/integrated/calculators/SalaryCalculator.kt
@@ -4,4 +4,7 @@ import com.codetutor.solidprinciples.integrated.Staff
 
 interface SalaryCalculator {
     fun calculateSalary(staff: Staff): Double
+    fun generatePaySlip(staff: Staff): String
+    fun applyBonus(staff: Staff, bonus: Double): Double
+    fun deductTaxes(staff: Staff, tax: Double): Double
 }
